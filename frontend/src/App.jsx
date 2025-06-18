e65ee73eba453c633c4fc6c9f9a519021bc77db4
MadBoy <73926361+madboy482@users.noreply.github.com>
2025-06-18 16:30:46 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Onboarding from "./pages/Onboarding";
 import PodDashboard from "./pages/PodDashboard";
