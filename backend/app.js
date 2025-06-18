ba28c65a80be8419af1df205cf63795f62b413c6
MadBoy <73926361+madboy482@users.noreply.github.com>
2025-06-18 16:22:01 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,11 @@ const userRoutes = require("./routes/userRoutes");
 const feedbackRoutes = require("./routes/feedbackRoutes");
 const connectionRoutes = require("./routes/connectionRoutes");
 const analyticsRoutes = require("./routes/analyticsRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
@@ -19,9 +21,14 @@ app.use("/api/feedback", feedbackRoutes);
 app.use("/api/connections", connectionRoutes);
 app.use("/api/analytics", analyticsRoutes);
 app.use("/api/reflections", require("./routes/reflectionRoutes"));
+app.use("/api/messages", require("./routes/messageRoutes"));
 
 app.get("/", (req, res) => {
   res.send("Amor Fly Backend API is running");
 });
+
+// Error handling middleware
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
